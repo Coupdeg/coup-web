e6ba5f3f67362dae302f9c9ba6e9910fb52498cc
Narongsak <narongsak.chobsri@gmail.com>
2017-08-08 15:34:37 +0700

    Modify database config

--- config/settings.py
+++ config/settings.py
@@ -71,16 +71,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'coup_development',
-#         'USER': 'myprojectuser',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '5000'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coup_development',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5000'
+    }
+}
 
 
 # Password validation
