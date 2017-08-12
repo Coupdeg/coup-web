f106acded5728f2e859b75d70455a0bc7eb85109
Narongsak <narongsak.chobsri@gmail.com>
2017-08-12 22:39:48 +0700

    Add static path

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'coupdeg'))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
