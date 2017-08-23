e99aa867e9602e7939fc485e6f43abd96e8860f6
Narongsak <narongsak.chobsri@gmail.com>
2017-08-23 14:40:38 +0700

    Can add user to database

--- coupdeg/urls_user.py
+++ coupdeg/urls_user.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^register/', views.register, name="register"),
     url(r'^login/', views.login, name="login"),
-		# url(r'^$', views.user, name="user"),
+		url(r'^$', views.user, name="user"),
 ]
 
 if settings.DEBUG:
--- coupdeg/views.py
+++ coupdeg/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Image
+from .models import User
 
 def landing(request):
  	return render(request, 'landing/index.html')
@@ -21,6 +22,18 @@ def login(request):
 def register(request):
 	return render(request, 'user/register.html')
 
-# def user(request):
-# 	if request.method == 'POST':
-		
+def user(request):
+	if request.method == 'POST':
+		email = request.POST['email']
+		first_name = request.POST['firstname']
+		last_name = request.POST['lastname']
+		address = request.POST['address']
+		city = request.POST['city']
+		state = request.POST['state']
+		country = request.POST['country']
+		zip_code = request.POST['zip']
+		user = User(email=email, first_name=first_name, last_name=last_name,
+								address=address, city=city, state=state, country=country, zip_code=zip_code)
+		user.save()
+		return redirect('/')
+	
