8e9a1ddcf60b199d582289e3975f88bedb2c45e3
Narongsak <narongsak.chobsri@gmail.com>
2017-08-26 14:18:54 +0700

    Add each product page

--- coupdeg/urls_product.py
+++ coupdeg/urls_product.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from . import views_product as view
 
 urlpatterns = [
-		url(r'^$', views.product, name="product"),
+		url(r'^$', view.product, name="product"),
+        url(r'^(?P<product_id>[0-9]+)/$', view.details, name="details")
 ]
 
 if settings.DEBUG:
