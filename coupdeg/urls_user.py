c602b69e3c87ec04fe8a3c15f784678f1fdc1cc3
Narongsak <narongsak.chobsri@gmail.com>
2017-08-26 14:29:38 +0700

    Divide views

--- coupdeg/urls_user.py
+++ coupdeg/urls_user.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from . import views_user as views
 
 urlpatterns = [
     url(r'^register/', views.register, name="register"),
