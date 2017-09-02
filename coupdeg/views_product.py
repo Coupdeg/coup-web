1873a6d0b3acfed1edad3f0cd929ac7b348bc8c4
Narongsak <narongsak.chobsri@gmail.com>
2017-09-02 14:07:47 +0700

    Modify name details to detail

--- coupdeg/views_product.py
+++ coupdeg/views_product.py
@@ -10,7 +10,7 @@ def product(request):
 	}
 	return render(request, 'product/index.html', context)
 
-def details(request, product_id):
+def detail(request, product_id):
 	product = get_object_or_404(Product, pk=product_id)
 	image = Image.objects.filter(type_id = product_id)[0]
-	return render(request, 'product/details.html', { 'product': product, 'image': image })
+	return render(request, 'product/detail.html', { 'product': product, 'image': image })
