6c64e660b836a89534564011e28828ba02adbc1e
Narongsak <narongsak.chobsri@gmail.com>
2017-10-04 16:20:53 +0700

    Add js cart

---
+++ coupdeg/static/js/cart.js
@@ -0,0 +1,14 @@
+$(document).ready( ()=> {       
+	$('.add-to-cart').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+});
