8d407941c809d4745808ae44db833262bddce089
Narongsak <narongsak.chobsri@gmail.com>
2017-10-04 21:46:31 +0700

    Deploy heroku images

---
+++ coupdeg/staticfiles/js/cart.31524fb639ac.js
@@ -0,0 +1,23 @@
+$(document).ready( ()=> {       
+	$('.add-to-cart').on('click', function(event){
+		event.preventDefault();
+		$('.add-to-cart-pop').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.add-to-cart-pop').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.add-to-cart-pop') || $(event.target).is('.cd-popup-no') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+		if( $(event.target).is('.cd-popup-yes') ) {
+			console.log($(".cd-popup-yes input").val())
+			$.ajax({
+				url: '/product/cart',
+				data : {'product_id': $(".add-to-cart-pop .cd-popup-yes input").val() },
+			});
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+});
