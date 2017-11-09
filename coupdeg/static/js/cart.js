b1fcc7c8ed3218f471bfc531bb651d213aecb841
ntpbright <nuttapatprom.c@ku.th>
2017-11-09 22:39:55 +0700

    recover scss in product detail page

---
+++ coupdeg/static/js/cart.js
@@ -0,0 +1,26 @@
+$(document).ready( ()=> {       
+	$('.add-to-cart').on('click', function(event){
+		event.preventDefault();
+		$('.add-to-cart-pop').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.add-to-cart-pop').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.add-to-cart-pop') || $(event.target).is('.cd-popup-no') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	$('.open-cart').on('click', function(event){
+		event.preventDefault();
+		$('.open-cart').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.open-cart').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.add-to-cart-pop') || $(event.target).is('.cd-popup-no') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+});
