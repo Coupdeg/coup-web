bb2f28ea08a06e93b91175ac3ab91e1886bcf927
Narongsak <narongsak.chobsri@gmail.com>
2017-11-13 18:15:26 +0700

    Deploy heroku

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_imgur',
+    'django_imgur.apps.ImgurConfig',
 ]
 
 MIDDLEWARE = [
