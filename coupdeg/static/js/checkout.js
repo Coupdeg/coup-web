a81ef6dc05a7307f389337b471f49b0c821e6f09
ntpbright <nuttapatprom.c@ku.th>
2017-11-20 22:52:21 +0700

    finish part of js in checkout

--- coupdeg/static/js/checkout.js
+++ coupdeg/static/js/checkout.js
@@ -29,8 +29,71 @@ $(document).ready(function() {
     })
     
     $("#btn-next-to-second").click(function(){
-        $("#tab-second").removeClass("pointer-inner-white");
-        $("#tab-second").addClass("pointer-inner-gray");
+        $("#tab-first").removeClass("pointer-inner-gray text-white");
+        $("#tab-first").addClass("pointer-inner-white text-gray");
+        $("#tab-first-number").removeClass("border-white");
+        $("#tab-first-number").addClass("border-gray check-symbol-green");
+        $("#tab-first-number").text('✔');
+        $("#shopping_cart").removeClass("show active");
+        $("#shopping_cart").addClass("display-none");
+
+        $("#tab-second").removeClass("pointer-inner-white text-gray");
+        $("#tab-second").addClass("pointer-inner-gray text-white");
+        $("#tab-second-number").removeClass("border-gray");
+        $("#tab-second-number").addClass("border-white");        
+        $("#shopping_info").removeClass("display-none");
+        $("#shopping_info").addClass("show active");
+    });
+
+    $("#btn-next-to-third").click(function(){
+        $("#tab-second").removeClass("pointer-inner-gray text-white");
+        $("#tab-second").addClass("pointer-inner-white text-gray");
+        $("#tab-second-number").removeClass("border-white");
+        $("#tab-second-number").addClass("border-gray check-symbol-green");
+        $("#tab-second-number").text('✔');
+        $("#shopping_info").removeClass("show active");
+        $("#shopping_info").addClass("display-none");
+
+        $("#tab-third").removeClass("pointer-inner-last-white text-gray");
+        $("#tab-third").addClass("pointer-inner-last-gray text-white");
+        $("#tab-third-number").removeClass("border-gray");
+        $("#tab-third-number").addClass("border-white");
+        $("#shopping_payment").removeClass("display-none");
+        $("#shopping_payment").addClass("show active");
+    });
+
+    $("#btn-back-to-first").click(function(){
+        $("#tab-first").removeClass("pointer-inner-white text-gray");
+        $("#tab-first").addClass("pointer-inner-gray text-white");
+        $("#tab-first-number").removeClass("border-gray check-symbol-green");
+        $("#tab-first-number").addClass("border-white");
+        $("#tab-first-number").text('1');
+        $("#shopping_cart").removeClass("display-none");
+        $("#shopping_cart").addClass("show active");
+
+        $("#tab-second").removeClass("pointer-inner-gray text-white");
+        $("#tab-second").addClass("pointer-inner-white text-gray");
+        $("#tab-second-number").removeClass("border-gray check-symbol-green");
+        $("#tab-second-number").addClass("border-gray");
+        $("#shopping_info").removeClass("show active");
+        $("#shopping_info").addClass("display-none");
+    });
+
+    $("#btn-back-to-second").click(function(){
+        $("#tab-second").removeClass("pointer-inner-white text-gray");
+        $("#tab-second").addClass("pointer-inner-gray text-white");
+        $("#tab-second-number").removeClass("border-gray check-symbol-green");
+        $("#tab-second-number").addClass("border-white");
+        $("#tab-second-number").text('2');
+        $("#shopping_info").removeClass("display-none");
+        $("#shopping_info").addClass("show active");
+
+        $("#tab-third").removeClass("pointer-inner-last-gray text-white");
+        $("#tab-third").addClass("pointer-inner-last-white text-gray");
+        $("#tab-third-number").removeClass("border-gray ");
+        $("#tab-third-number").addClass("border-gray");
+        $("#shopping_payment").removeClass("show active");
+        $("#shopping_payment").addClass("display-none");
     });
 });
 
