0607d538fe6fb25a135e48d7bf12a17325d634fb
Narongsak <narongsak.chobsri@gmail.com>
2017-11-30 12:58:32 +0700

    Add function and create ui search

--- coupdeg/urls_product.py
+++ coupdeg/urls_product.py
@@ -9,7 +9,8 @@ urlpatterns = [
         url(r'^(?:type=(?P<type_number>[0-6]+))/$', view.product, name="product"),
 		url(r'^add_to_cart=(?P<product_id>[0-9]+)', view.add_to_cart, name="add_to_cart"),
         url(r'^remove_from_cart=(?P<product_id>[0-9]+)', view.remove_from_cart, name="remove_from_cart"),
-        url(r'^update_from_cart=(?P<product_id>[0-9]+)', view.update_from_cart, name="update_from_cart"),                                
+        url(r'^update_from_cart=(?P<product_id>[0-9]+)', view.update_from_cart, name="update_from_cart"),
+        url(r'^search/', view.search, name="search"),                                
 ]
 
 if settings.DEBUG:
